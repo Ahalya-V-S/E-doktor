1b9b448918f44e637890985fe2faaee29e76d0d6
Ahalya Sakthivelu <115523634+Ahalya-V-S@users.noreply.github.com>
2023-04-08 14:04:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function test(){
+    var ag=document.getElementById('age').value;
+    var di=document.getElementById('disease').value;
+    if (di=='GID'){
+        window.close('index.html');
+        window.open('GID.html');
+    }
+    if (di=='ND'){
+        window.close('index.html');
+        window.open('ND.html');
+    }
+    if (di=='MD'){
+        window.close('index.html');
+        window.open('MD.html');
+    }
+    if (di=='PD'){
+        window.close('index.html');
+        window.open('PD.html');
+    }
+    if (di=='NPD'){
+        window.close('index.html');
+        window.open('NPD.html');
+    }
+    if (di=='RD'){
+        window.close('index.html');
+        window.open('RD.html');
+    }
+    if (di=='GD'){
+        window.close('index.html');
+        window.open('GD.html');
+    }
+    if (di=='STD'){
+        window.close('index.html');
+        window.open('STD.html');
+    }
+    if (di=='CVD'){
+        window.close('index.html');
+        window.open('CVD.html');
+    }
+}
